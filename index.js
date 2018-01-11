a60aeaee334401e13e1b5855545e707dc34093b1
Ashley Casey <acasey.dev@gmail.com>
2018-01-11 15:40:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  let newArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  newArray;
+}
